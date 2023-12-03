7211fb2fa68e9b2218780b37f720671f862e3705
abdulrawoof <aiyubalikhan92@gmail.com>
2023-12-03 14:59:29 +0530

    delete

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,8 +1,8 @@
-# variable "aws_region" {
-#   description = "AWS Region"
-#   type        = string
-#   default     = "us-east-1"
-# }
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
 
 
 # variable "vpc_name" {
